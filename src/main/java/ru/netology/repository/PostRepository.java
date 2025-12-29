e6ddda29af87629ab978773da8a3a32072f52f7a
ksa237 <ksa237@yandex.ru>
2025-12-30 01:09:52 +1000

    di-annotation result

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -2,48 +2,17 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 
-// Stub
-public class PostRepository {
+public interface PostRepository {
 
-    private CopyOnWriteArrayList<Post> webData = new CopyOnWriteArrayList<>();
-    //private ConcurrentHashMap<Long, Post> webData = new ConcurrentHashMap<>();
+    List<Post> all();
 
+    Optional<Post> getById(long id);
 
-    public List<Post> all() {
-        //ksa237++
-        return webData;
-        //return Collections.emptyList();
-        //ksa237--
-    }
+    Post save(Post post);
 
-    public Optional<Post> getById(long id) {
-        //ksa237++
-        var post = webData.get((int) id);
+    void removeById(long id);
 
-        if (post != null) {
-            return Optional.of(post);
-        }
-        return Optional.empty();
-        //ksa237--
-    }
-
-    public Post save(Post post) {
-        //ksa237++
-        //считаем что нужно записать новый пост.
-        //добавляем данные в конец списка, получаем присовенный id для нового поста
-        webData.add(post);
-        post.setId(webData.size());
-        //ksa237--
-
-        return post;
-    }
-
-    public void removeById(long id) {
-    }
 }
--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,8 +1,7 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.netology.controller.PostController;
-import ru.netology.repository.PostRepository;
-import ru.netology.service.PostService;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,34 +12,45 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-        final var repository = new PostRepository();
-        final var service = new PostService(repository);
-        controller = new PostController(service);
+        final var context = new AnnotationConfigApplicationContext("ru.netology");
+        this.controller = context.getBean("postController", PostController.class);
     }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) {
-        // если деплоились в root context, то достаточно этого
+
         try {
             final var path = req.getRequestURI();
             final var method = req.getMethod();
-            // primitive routing
+
             if (method.equals("GET") && path.equals("/api/posts")) {
                 controller.all(resp);
                 return;
             }
             if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
-                // easy way
+
                 final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
                 controller.getById(id, resp);
                 return;
             }
             if (method.equals("POST") && path.equals("/api/posts")) {
+                /*
+                для целей тестирования функционала использую json запись,
+                отправляемую через дополнительное ПО Insomnia (версия для Линукс)
+
+                body в request выглядит так:
+
+                {
+                    "id": 0,
+                        "content": "Hello, \"world\"!\nNew string."
+                }
+
+                */
                 controller.save(req.getReader(), resp);
                 return;
             }
             if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
-                // easy way
+
                 final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
                 controller.removeById(id, resp);
                 return;
